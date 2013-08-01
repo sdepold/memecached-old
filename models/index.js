7f9408add6a178e7a8fb5ef9d36db8d5af44f7fa
Sascha Depold <sascha@depold.com>
2013-08-01 07:37:11 +0200

    updated color of pg db

--- models/index.js
+++ models/index.js
@@ -2,9 +2,9 @@ if (!global.hasOwnProperty('db')) {
   var Sequelize = require('sequelize')
     , sequelize = null
 
-  if (process.env.HEROKU_POSTGRESQL_BRONZE_URL) {
+  if (process.env.HEROKU_POSTGRESQL_GRAY_URL) {
     // the application is executed on Heroku ... use the postgres database
-    var match = process.env.HEROKU_POSTGRESQL_BRONZE_URL.match(/postgres:\/\/([^:]+):([^@]+)@([^:]+):(\d+)\/(.+)/)
+    var match = process.env.HEROKU_POSTGRESQL_GRAY_URL.match(/postgres:\/\/([^:]+):([^@]+)@([^:]+):(\d+)\/(.+)/)
 
     sequelize = new Sequelize(match[5], match[1], match[2], {
       dialect:  'postgres',
